12a3c312cf6d8731b046deb7b6657e0026dda7ae
Sandra <sandrabess@hotmail.com>
2020-11-05 13:41:40 -0500

    exercise 1 completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Raleway', sans-serif;
+  }
+
+  body {
+    margin: 0px;
+  }
+`;
+
 
 const App = (props) => {
-  return 'Hello world!';
+  return (
+  <Router>
+     <GlobalStyle />
+     <Header/>
+    <Switch>
+      <Route path="/">Homepage</Route>
+      <Route path="/about">About</Route>
+    </Switch>
+  </Router>);
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  padding: 0 15px;
+  color: blueViolet;
+  font-weight: bold;
+`;
+
+const Header = (props)=> {
+    return (
+        <Wrapper>
+        <h1>Fruit Emporium</h1>
+        <nav>
+            <StyledLink to="/">
+                Home
+            </StyledLink>
+            <StyledLink to="/about">
+                About
+            </StyledLink>
+        </nav>
+        </Wrapper>
+    )
+}
+
+export default Header;
