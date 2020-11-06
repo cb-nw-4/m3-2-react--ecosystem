7941add5c210c74c942eba6e0f879c67e9d7041e
Feriel Nabet <fnabet@ipconnex.com>
2020-11-06 16:30:09 -0500

    Update app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import Header from './Header';
+import { BrowserRouter , Switch, Route} from "react-router-dom";
+import AboutPage from './AboutPage';
+import HomePage from './HomePage';
+import ItemsDetails from './ItemsDetails';
+import SellersDetails from './SellersDetails';
+
+
 
 const App = (props) => {
-  return 'Hello world!';
+  return (
+    <>
+      <BrowserRouter>
+        <Header/>
+        <div className="content" >
+          
+            <Switch>
+              <Route exact path="/sellers/:sellerId"  >
+                <SellersDetails/>
+              </Route>
+              <Route exact path="/items/:itemId"  >
+                <ItemsDetails/>
+              </Route>
+              <Route exact path="/about">
+                <AboutPage /> 
+              </Route>
+              <Route exact path="/" >
+                <HomePage /> 
+              </Route>
+                              
+            </Switch>
+          
+         
+        </div>
+        
+      </BrowserRouter>
+     
+    </>
+  );
 };
 
 export default App;
