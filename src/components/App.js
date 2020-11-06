a9a5e39186d1752e1ffd76bd62adf5c76dc5e940
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-11-05 21:09:35 -0500

    Updated App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import GlobalStyles from './GlobalStyles';
+
+import Header from './Header';
+import About from './About';
+import Homepage from './Home';
+import ItemDetails from './ItemDetails';
+
 const App = (props) => {
-  return 'Hello world!';
+  return (
+    <Router>
+    <Header></Header>
+    <Switch>
+      <Route path='/items/:itemID'><ItemDetails /></Route>
+      <Route path='/about'><About /></Route>
+      <Route path='/'><Homepage /></Route>
+    </Switch>
+    <GlobalStyles/>
+    </Router>
+  );
 };
 
 export default App;
