11990308bb720f7ac36c49f8c4a3699a7c4b413e
“Winnie <chanwinnie.wc@gmail.com>
2020-11-07 14:35:05 -0500

    Exercise 1-3 completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import ItemDetails from './ItemDetails';
 
-const App = (props) => {
-  return 'Hello world!';
+
+const App = () => {
+  return (
+      <Router>
+        <>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/items/:itemId">
+              <ItemDetails />
+            </Route>
+          </Switch>
+        </>
+      </Router>
+      
+  )
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+//import data from '../data';
 import App from './components/App';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(<App />, rootElement);
+//items={data.items}
